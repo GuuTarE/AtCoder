2b9103d09f52ae2c732e758f9767798fe228637e
Hikaru Matsuoka <hikarumatsuoka@Hikaru-no-MacBook-Air.local>
2021-03-23 23:23:13 +0900

    add BitFS

---
+++ BitFS.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+/*
+** bit全探索の雛形
+
+*/
+
+/* Debug */
+int main(){
+
+    int K; //組み合わせの長さ
+
+    for(int bit=0; bit<(1<<K); bit++){
+        vi vec(N, 0);
+        REP(i, 0, K){
+            if(bit & (1<<i)){
+                /*1の場合の処理*/
+            }
+        }
+    }
+    return 0;
+}
+
